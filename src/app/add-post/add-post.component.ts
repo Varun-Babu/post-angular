eaf2fcbf8278c53850b0de757796e0fcc670d3e1
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-29 07:01:44 +0530

    databinded

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent {
+name=""
+id=""
+title=""
+status=""
+
+readValue = () =>
+{
+  let data:any = {"name":this.name,"id":this.id,"title":this.title,"status":this.status}
+  console.log(data)
+}
 
 }
