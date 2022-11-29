187e8b5902e57e00790b27d4c01c3833201f604e
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-29 14:29:19 +0530

    service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ViewPostComponent } from './view-post/view-post.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 const myRoute : Routes=[
   {
@@ -30,7 +31,8 @@ const myRoute : Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
